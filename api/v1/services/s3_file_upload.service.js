cefae2779344afb7e98de5b77a03d96cfe7a7b2c
Mohammad Areeb Siddiqui <mohammadareebs@gmail.com>
2020-11-10 01:30:46 +0500

    Remove unused log from s3 file upload service

--- api/v1/services/s3_file_upload.service.js
+++ api/v1/services/s3_file_upload.service.js
@@ -4,7 +4,6 @@ const { S3Object, upload_to_s3 } = require("../../utils/s3.init");
 class S3FileUploadService {
   async upload_file(key, file) {
     try {
-      console.log("asd");
       const buff = get_file_buffer(file["value"]);
       const data = await upload_to_s3(new S3Object(key, buff, file["type"]));
       
