9a833f6377158d833eaf3baeca82d10546fcc21d
Mohammad Areeb Siddiqui <mohammadareebs@gmail.com>
2020-11-30 15:42:45 +0500

    Remove classroom model from index of models

--- api/models/index.js
+++ api/models/index.js
@@ -2,7 +2,6 @@ const { User } = require("./user.model");
 const { Admin } = require("./admin.model");
 const { Institute } = require("./institute.model");
 const { Qari } = require("./qari.model");
-const { Classroom } = require("./classroom.model");
 const { Student } = require("./student.model");
 const { Booking } = require("./booking.model");
 const { Session } = require("./session.model");
@@ -12,7 +11,6 @@ module.exports = {
     Admin,
     Institute,
     Qari,
-    Classroom,
     Student,
     Booking,
     Session
